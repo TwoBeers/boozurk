9139ef9aa24181c15171df7df28eb28146ecdffc
jimo <jimoko@libero.it>
2013-12-03 17:25:03 +0100

    3.00 20131203
    small css/js fixes

--- js/layout.js
+++ js/layout.js
@@ -7,16 +7,16 @@ jQuery(document).ready(function($){
 
 	if ($head_wrap.length) {
 		$head_wrap.addClass('fixed');
-		window.onresize = function() {
+		$(window).resize(function() {
 			$posts_content.css( {'padding-top' : $head_wrap.height() } );
-			$head_wrap.css( {'width' : $posts_content.width() - 1 } )
-		}
+			$head_wrap.css( {'width' : $posts_content.width() - 1 } );
+		});
 	}
 
 	if ($sidebar_fixed_viewport.length) {
-		window.onresize = function() {
+		$(window).resize(function() {
 			$sidebar_fixed_viewport.css( {'height' : $(window).height() - $content_offset.top } );
-		}
+		});
 	}
 
 });
